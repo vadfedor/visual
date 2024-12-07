fff2b6be3db394c5aec65f54356a31efdca1a1a7
vadfedor <vif7@tpu.ru>
2024-12-07 11:25:28 +0700

    main-cpp-change

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -37,7 +37,7 @@ int main() {
 			cin >> n;
 			n = floor(pow(2, n)); // Получаем целочисленное число 2^n
 			string array[16] = { "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E" };
-			cout << "OK\n";
+			cout << "OK\nHi again!\n";
 			break;
 		}
 		}
